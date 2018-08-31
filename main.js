6b7859124b0689b35c1854c415d06acfe564aef9
Sean Hong <writeseanhong@gmail.com>
2018-08-31 15:29:34 -0500

    completed basic logic and gameplay

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
+window.addEventListener("load", init);
+
 //Globals
-let time = 5;
+
+// Available Levels
+const levels = {
+  easy: 5,
+  medium: 3,
+  hard: 1
+};
+
+// to change level
+const currentLevel = levels.easy;
+
+let time = currentLevel;
 let score = 0;
 let isPlaying;
 
@@ -114,8 +127,73 @@ const words = [
   "porchetta",
   "cupim"
 ];
-
 //Initialize Game
 function init() {
-  console.log("init");
+  showWord(words);
+  //Start matching on word input
+  wordInput.addEventListener("input", startMatch);
+  //check timer
+  setInterval(countdown, 1000);
+  //check game status
+  setInterval(checkStatus, 50);
+}
+
+// start match function
+function startMatch() {
+  if (matchWords()) {
+    isPlaying = true;
+    time = currentLevel + 1;
+    showWord(words);
+    wordInput.value = "";
+    score++;
+  }
+  // if score is -1, display 0
+  if (score === -1) {
+    scoreDisplay.innerHTML = 0;
+  } else {
+    scoreDisplay.innerHTML = score;
+  }
+}
+
+function matchWords() {
+  if (wordInput.value === currentWord.innerHTML) {
+    message.innerHTML = "Correct!!!";
+    return true;
+  } else {
+    message.innerHTML = "";
+    return false;
+  }
+}
+
+//Match currentWord to wordInput
+
+//pick and show a random word
+function showWord(words) {
+  // generate random array index
+  const randIndex = Math.floor(Math.random() * words.length);
+
+  // output random word
+  currentWord.innerHTML = words[randIndex];
+}
+
+function countdown() {
+  // confirm time doesn't run out
+  if (time > 0) {
+    //decrement timer
+    time--;
+  } else if (time === 0) {
+    //game over
+    isPlaying = false;
+  }
+
+  //show time
+  timeDisplay.innerHTML = time;
+}
+
+// check game status
+function checkStatus() {
+  if (!isPlaying && time === 0) {
+    message.innerHTML = "Game Over!!!";
+    score = -1;
+  }
 }
